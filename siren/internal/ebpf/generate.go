c3fb4f9ba4f9551c893d515efad560cf9a68cdad
bytemomo <82763502+bytemomo@users.noreply.github.com>
2025-11-07 16:21:48 +0100

    fix: payload when logging

--- siren/internal/ebpf/generate.go
+++ siren/internal/ebpf/generate.go
@@ -1,3 +1,4 @@
 package ebpf
 
 //go:generate bash -c "cd program && clang -O2 -g -Wall -target bpf -c xdp_proxy.c -o xdp_proxy.bpf.o"
+//go:generate bash -c "cd program && clang -O2 -g -Wall -target bpf -c synth.c -o synth.bpf.o"
