5b675bd269fffef19e5d971585f0a30c51fdfaca
bytemomo <82763502+bytemomo@users.noreply.github.com>
2025-11-17 10:38:38 +0100

    feat: telnet

--- kraken/internal/modules/entry.go
+++ kraken/internal/modules/entry.go
@@ -3,9 +3,11 @@ package modules
 import (
 	"bytemomo/kraken/internal/modules/mqtt"
 	"bytemomo/kraken/internal/modules/rtsp"
+	"bytemomo/kraken/internal/modules/telnet"
 )
 
 func Init() {
 	mqtt.Init()
 	rtsp.Init()
+	telnet.Init()
 }
