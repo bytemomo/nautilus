9f1a4c1a0201728327e7cc2da8bb1fafa04b60fc
bytemomo <82763502+bytemomo@users.noreply.github.com>
2025-11-20 09:11:00 +0100

    fix: add run.sh file

--- modules/kraken/fuzz/mqtt/wolfmqtt_afl/harness.c
+++ modules/kraken/fuzz/mqtt/wolfmqtt_afl/harness.c
@@ -4,111 +4,262 @@
 #include <string.h>
 #include <unistd.h>
 
-#include "wolfmqtt/mqtt_packet.h"
+#include "wolfmqtt/mqtt_client.h"
 
 __AFL_FUZZ_INIT();
 
-#if defined(WOLFMQTT_V5)
-static void free_props(MqttProp *props) {
+#ifdef WOLFMQTT_V5
+static int Handle_Props(MqttClient *client, MqttProp *props, byte use_cb,
+                        byte free_props) {
+    int rc = MQTT_CODE_SUCCESS;
+
     if (props != NULL) {
-        (void)MqttProps_Free(props);
-    }
-}
+#ifdef WOLFMQTT_PROPERTY_CB
+        if ((use_cb == 1) && (client->property_cb != NULL)) {
+            int rc_err = client->property_cb(client, props,
+                                             client->property_ctx);
+            if (rc_err < 0) {
+                rc = rc_err;
+            }
+        }
 #else
-static void free_props(void *props) {
-    (void)props;
-}
+        (void)client;
+        (void)use_cb;
 #endif
-
-static void decode_connect_ack(byte *buf, int len) {
-    MqttConnectAck ack;
-    memset(&ack, 0, sizeof(ack));
-#if defined(WOLFMQTT_V5)
-    ack.protocol_level = MQTT_CONNECT_PROTOCOL_LEVEL_5;
-#endif
-    if (MqttDecode_ConnectAck(buf, len, &ack) >= 0) {
-#if defined(WOLFMQTT_V5)
-        free_props(ack.props);
-#endif
+        if (free_props) {
+            MqttProps_Free(props);
+        }
     }
+    return rc;
 }
+#endif
 
-static void decode_publish(byte *buf, int len) {
-    MqttPublish publish;
-    memset(&publish, 0, sizeof(publish));
-#if defined(WOLFMQTT_V5)
-    publish.protocol_level = MQTT_CONNECT_PROTOCOL_LEVEL_5;
-#endif
-    if (MqttDecode_Publish(buf, len, &publish) >= 0) {
-#if defined(WOLFMQTT_V5)
-        free_props(publish.props);
-#endif
+static int MqttClient_DecodePacket(MqttClient *client, byte *rx_buf,
+                                   word32 rx_len, void *packet_obj,
+                                   MqttPacketType *ppacket_type,
+                                   MqttQoS *ppacket_qos, word16 *ppacket_id,
+                                   int doProps) {
+    int rc = MQTT_CODE_SUCCESS;
+    MqttPacket *header;
+    MqttPacketType packet_type;
+    MqttQoS packet_qos;
+    word16 packet_id = 0;
+
+    if (rx_buf == NULL || rx_len < MQTT_PACKET_HEADER_MIN_SIZE) {
+        return MQTT_TRACE_ERROR(MQTT_CODE_ERROR_BAD_ARG);
     }
-}
 
-static void decode_publish_resp(byte *buf, int len, MqttPacketType type) {
-    MqttPublishResp resp;
-    memset(&resp, 0, sizeof(resp));
-#if defined(WOLFMQTT_V5)
-    resp.protocol_level = MQTT_CONNECT_PROTOCOL_LEVEL_5;
-#endif
-    if (MqttDecode_PublishResp(buf, len, (byte)type, &resp) >= 0) {
-#if defined(WOLFMQTT_V5)
-        free_props(resp.props);
-#endif
+    header = (MqttPacket *)rx_buf;
+    packet_type = (MqttPacketType)MQTT_PACKET_TYPE_GET(header->type_flags);
+    if (ppacket_type) {
+        *ppacket_type = packet_type;
     }
-}
-
-static void decode_subscribe_ack(byte *buf, int len) {
-    MqttSubscribeAck ack;
-    memset(&ack, 0, sizeof(ack));
-#if defined(WOLFMQTT_V5)
-    ack.protocol_level = MQTT_CONNECT_PROTOCOL_LEVEL_5;
-#endif
-    if (MqttDecode_SubscribeAck(buf, len, &ack) >= 0) {
-#if defined(WOLFMQTT_V5)
-        free_props(ack.props);
-#endif
+    packet_qos = (MqttQoS)MQTT_PACKET_FLAGS_GET_QOS(header->type_flags);
+    if (ppacket_qos) {
+        *ppacket_qos = packet_qos;
     }
-}
 
-static void decode_unsubscribe_ack(byte *buf, int len) {
-    MqttUnsubscribeAck ack;
-    memset(&ack, 0, sizeof(ack));
-#if defined(WOLFMQTT_V5)
-    ack.protocol_level = MQTT_CONNECT_PROTOCOL_LEVEL_5;
+    if (ppacket_id || packet_obj) {
+        switch (packet_type) {
+        case MQTT_PACKET_TYPE_CONNECT_ACK: {
+            MqttConnectAck connect_ack, *p_connect_ack = &connect_ack;
+            if (packet_obj) {
+                p_connect_ack = (MqttConnectAck *)packet_obj;
+            } else {
+                XMEMSET(p_connect_ack, 0, sizeof(MqttConnectAck));
+            }
+#ifdef WOLFMQTT_V5
+            p_connect_ack->protocol_level = client->protocol_level;
 #endif
-    if (MqttDecode_UnsubscribeAck(buf, len, &ack) >= 0) {
-#if defined(WOLFMQTT_V5)
-        free_props(ack.props);
+            rc = MqttDecode_ConnectAck(rx_buf, rx_len, p_connect_ack);
+#ifdef WOLFMQTT_V5
+            if (rc >= 0 && doProps) {
+                int tmp = Handle_Props(client, p_connect_ack->props,
+                                       (packet_obj != NULL), 1);
+                p_connect_ack->props = NULL;
+                if (tmp != MQTT_CODE_SUCCESS) {
+                    rc = tmp;
+                }
+            }
 #endif
-    }
-}
-
-static void decode_ping(byte *buf, int len) {
-    MqttPing ping;
-    memset(&ping, 0, sizeof(ping));
-    (void)MqttDecode_Ping(buf, len, &ping);
-}
-
-#if defined(WOLFMQTT_V5)
-static void decode_disconnect(byte *buf, int len) {
-    MqttDisconnect disc;
-    memset(&disc, 0, sizeof(disc));
-    disc.protocol_level = MQTT_CONNECT_PROTOCOL_LEVEL_5;
-    if (MqttDecode_Disconnect(buf, len, &disc) >= 0) {
-        free_props(disc.props);
-    }
-}
-
-static void decode_auth(byte *buf, int len) {
-    MqttAuth auth;
-    memset(&auth, 0, sizeof(auth));
-    if (MqttDecode_Auth(buf, len, &auth) >= 0) {
-        free_props(auth.props);
-    }
-}
+            break;
+        }
+        case MQTT_PACKET_TYPE_PUBLISH: {
+            MqttPublish publish, *p_publish;
+            if (packet_obj) {
+                p_publish = (MqttPublish *)packet_obj;
+#ifdef WOLFMQTT_V5
+                p_publish->protocol_level = client->protocol_level;
 #endif
+            } else {
+                p_publish = &publish;
+                XMEMSET(p_publish, 0, sizeof(MqttPublish));
+            }
+            rc = MqttDecode_Publish(rx_buf, rx_len, p_publish);
+            if (rc >= 0) {
+                packet_id = p_publish->packet_id;
+#ifdef WOLFMQTT_V5
+                if (doProps) {
+                    int tmp = Handle_Props(client, p_publish->props,
+                                           (packet_obj != NULL), 0);
+                    if (tmp != MQTT_CODE_SUCCESS) {
+                        rc = tmp;
+                    }
+                }
+#endif
+            }
+            break;
+        }
+        case MQTT_PACKET_TYPE_PUBLISH_ACK:
+        case MQTT_PACKET_TYPE_PUBLISH_REC:
+        case MQTT_PACKET_TYPE_PUBLISH_REL:
+        case MQTT_PACKET_TYPE_PUBLISH_COMP: {
+            MqttPublishResp publish_resp, *p_publish_resp = &publish_resp;
+            if (packet_obj) {
+                p_publish_resp = (MqttPublishResp *)packet_obj;
+            } else {
+                XMEMSET(p_publish_resp, 0, sizeof(MqttPublishResp));
+            }
+
+#ifdef WOLFMQTT_V5
+            p_publish_resp->protocol_level = client->protocol_level;
+#endif
+            rc = MqttDecode_PublishResp(rx_buf, rx_len, packet_type,
+                                        p_publish_resp);
+            if (rc >= 0) {
+                packet_id = p_publish_resp->packet_id;
+#ifdef WOLFMQTT_V5
+                if (doProps) {
+                    int tmp = Handle_Props(client, p_publish_resp->props,
+                                           (packet_obj != NULL), 1);
+                    p_publish_resp->props = NULL;
+                    if (tmp != MQTT_CODE_SUCCESS) {
+                        rc = tmp;
+                    }
+                }
+#endif
+            }
+            break;
+        }
+        case MQTT_PACKET_TYPE_SUBSCRIBE_ACK: {
+            MqttSubscribeAck subscribe_ack, *p_subscribe_ack = &subscribe_ack;
+            if (packet_obj) {
+                p_subscribe_ack = (MqttSubscribeAck *)packet_obj;
+            } else {
+                XMEMSET(p_subscribe_ack, 0, sizeof(MqttSubscribeAck));
+            }
+#ifdef WOLFMQTT_V5
+            p_subscribe_ack->protocol_level = client->protocol_level;
+#endif
+            rc = MqttDecode_SubscribeAck(rx_buf, rx_len, p_subscribe_ack);
+            if (rc >= 0) {
+                packet_id = p_subscribe_ack->packet_id;
+#ifdef WOLFMQTT_V5
+                if (doProps) {
+                    int tmp = Handle_Props(client, p_subscribe_ack->props,
+                                           (packet_obj != NULL), 1);
+                    p_subscribe_ack->props = NULL;
+                    if (tmp != MQTT_CODE_SUCCESS) {
+                        rc = tmp;
+                    }
+                }
+#endif
+            }
+            break;
+        }
+        case MQTT_PACKET_TYPE_UNSUBSCRIBE_ACK: {
+            MqttUnsubscribeAck unsubscribe_ack,
+                *p_unsubscribe_ack = &unsubscribe_ack;
+            if (packet_obj) {
+                p_unsubscribe_ack = (MqttUnsubscribeAck *)packet_obj;
+            } else {
+                XMEMSET(p_unsubscribe_ack, 0, sizeof(MqttUnsubscribeAck));
+            }
+#ifdef WOLFMQTT_V5
+            p_unsubscribe_ack->protocol_level = client->protocol_level;
+#endif
+            rc = MqttDecode_UnsubscribeAck(rx_buf, rx_len, p_unsubscribe_ack);
+            if (rc >= 0) {
+                packet_id = p_unsubscribe_ack->packet_id;
+#ifdef WOLFMQTT_V5
+                if (doProps) {
+                    int tmp = Handle_Props(client, p_unsubscribe_ack->props,
+                                           (packet_obj != NULL), 1);
+                    p_unsubscribe_ack->props = NULL;
+                    if (tmp != MQTT_CODE_SUCCESS) {
+                        rc = tmp;
+                    }
+                }
+#endif
+            }
+            break;
+        }
+        case MQTT_PACKET_TYPE_PING_RESP: {
+            MqttPing ping, *p_ping = &ping;
+            if (packet_obj) {
+                p_ping = (MqttPing *)packet_obj;
+            } else {
+                XMEMSET(p_ping, 0, sizeof(MqttPing));
+            }
+            rc = MqttDecode_Ping(rx_buf, rx_len, p_ping);
+            break;
+        }
+        case MQTT_PACKET_TYPE_AUTH: {
+#ifdef WOLFMQTT_V5
+            MqttAuth auth, *p_auth = &auth;
+            if (packet_obj) {
+                p_auth = (MqttAuth *)packet_obj;
+            } else {
+                XMEMSET(p_auth, 0, sizeof(MqttAuth));
+            }
+            rc = MqttDecode_Auth(rx_buf, rx_len, p_auth);
+            if (rc >= 0 && doProps) {
+                int tmp = Handle_Props(client, p_auth->props,
+                                       (packet_obj != NULL), 1);
+                p_auth->props = NULL;
+                if (tmp != MQTT_CODE_SUCCESS) {
+                    rc = tmp;
+                }
+            }
+#else
+            rc = MQTT_TRACE_ERROR(MQTT_CODE_ERROR_PACKET_TYPE);
+#endif
+            break;
+        }
+        case MQTT_PACKET_TYPE_DISCONNECT: {
+#ifdef WOLFMQTT_V5
+            MqttDisconnect disc, *p_disc = &disc;
+            if (packet_obj) {
+                p_disc = (MqttDisconnect *)packet_obj;
+            } else {
+                XMEMSET(p_disc, 0, sizeof(MqttDisconnect));
+            }
+            rc = MqttDecode_Disconnect(rx_buf, rx_len, p_disc);
+            if (rc >= 0 && doProps) {
+                int tmp = Handle_Props(client, p_disc->props,
+                                       (packet_obj != NULL), 1);
+                p_disc->props = NULL;
+                if (tmp != MQTT_CODE_SUCCESS) {
+                    rc = tmp;
+                }
+            }
+#else
+            rc = MQTT_TRACE_ERROR(MQTT_CODE_ERROR_PACKET_TYPE);
+#endif
+            break;
+        }
+        default:
+            rc = MQTT_TRACE_ERROR(MQTT_CODE_ERROR_PACKET_TYPE);
+            break;
+        }
+    }
+
+    if (ppacket_id) {
+        *ppacket_id = packet_id;
+    }
+
+    return rc;
+}
 
 static void fuzz_one(const uint8_t *data, size_t size) {
     if (size < MQTT_PACKET_HEADER_MIN_SIZE || size > (size_t)INT_MAX) {
@@ -121,23 +272,19 @@ static void fuzz_one(const uint8_t *data, size_t size) {
     }
     memcpy(mutable_buf, data, size);
 
-    decode_connect_ack(mutable_buf, (int)size);
-    decode_publish(mutable_buf, (int)size);
-    decode_publish_resp(mutable_buf, (int)size,
-                        MQTT_PACKET_TYPE_PUBLISH_ACK);
-    decode_publish_resp(mutable_buf, (int)size,
-                        MQTT_PACKET_TYPE_PUBLISH_REC);
-    decode_publish_resp(mutable_buf, (int)size,
-                        MQTT_PACKET_TYPE_PUBLISH_REL);
-    decode_publish_resp(mutable_buf, (int)size,
-                        MQTT_PACKET_TYPE_PUBLISH_COMP);
-    decode_subscribe_ack(mutable_buf, (int)size);
-    decode_unsubscribe_ack(mutable_buf, (int)size);
-    decode_ping(mutable_buf, (int)size);
+    MqttClient client;
+    memset(&client, 0, sizeof(client));
 #if defined(WOLFMQTT_V5)
-    decode_disconnect(mutable_buf, (int)size);
-    decode_auth(mutable_buf, (int)size);
+    client.protocol_level = MQTT_CONNECT_PROTOCOL_LEVEL_5;
 #endif
+    MqttObject obj;
+    memset(&obj, 0, sizeof(obj));
+    MqttPacketType type = MQTT_PACKET_TYPE_RESERVED;
+    MqttQoS qos = MQTT_QOS_0;
+    word16 packet_id = 0;
+
+    (void)MqttClient_DecodePacket(&client, mutable_buf, (word32)size, &obj,
+                                  &type, &qos, &packet_id, 1);
 
     free(mutable_buf);
 }
---
+++ modules/kraken/fuzz/mqtt/wolfmqtt_afl/run.sh
@@ -0,0 +1,4 @@
+podman run --rm -it \
+    -v "../../seeds/mqtt/:/work/seeds" \
+    -v "./my_output/:/work/output" \
+    aflpp-wolfmqtt:latest
