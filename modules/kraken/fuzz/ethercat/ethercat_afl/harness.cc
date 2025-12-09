119c700b715a0e84a9864ca12dd4bc81cbf1ca66
bytemomo <82763502+bytemomo@users.noreply.github.com>
2025-12-09 16:22:25 +0100

    fix: wokring ethercat fuzzer (etherlab data parsing function) + ethercat's afl seeds

--- modules/kraken/fuzz/ethercat/ethercat_afl/harness.cc
+++ modules/kraken/fuzz/ethercat/ethercat_afl/harness.cc
@@ -1,46 +1,41 @@
 #include <stdint.h>
 #include <stdlib.h>
 #include <string.h>
-#include <stdarg.h>
 #include <stdio.h>
 #include <errno.h>
 
 #include "ecrt.h"
 
-extern "C" {
-#include "master.h"
-#include "domain.h"
-#include "slave_config.h"
-}
-
-extern "C" int ioctl(int fd, unsigned long request, ...) {
-    (void)fd; (void)request;
-    return 0;
-}
-
 extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
-    if (size < 16) return 0;
+    if (size < 16) {
+        return 0;
+    }
 
-    ec_master_t master;
-    ec_domain_t *domain = NULL;
-    ec_slave_config_t *sc = NULL;
+    // ecrt_request_master(0) will now use the fake library
+    ec_master_t *master = ecrt_request_master(0);
+    if (!master) {
+        return 0;
+    }
 
-    memset(&master, 0, sizeof(ec_master_t));
-    master.fd = 1;
+    ec_domain_t *domain = ecrt_master_create_domain(master);
+    if (!domain) {
+        ecrt_release_master(master);
+        return 0;
+    }
 
     uint16_t alias = *((uint16_t *)(data));
     uint16_t pos   = *((uint16_t *)(data + 2));
     uint32_t vend  = *((uint32_t *)(data + 4));
     uint32_t prod  = *((uint32_t *)(data + 8));
 
+    ec_slave_config_t *sc = ecrt_master_slave_config(
+            master, alias, pos, vend, prod);
+    if (!sc) {
+        ecrt_release_master(master);
+        return 0;
+    }
+
     size_t cursor = 12;
-
-    domain = ecrt_master_create_domain(&master);
-    if (!domain) goto cleanup;
-
-    sc = ecrt_master_slave_config(&master, alias, pos, vend, prod);
-    if (!sc) goto cleanup;
-
     while (cursor + 4 < size) {
         uint16_t index = *((uint16_t *)(data + cursor));
         uint8_t  sub   = data[cursor + 2];
@@ -48,10 +43,6 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
         cursor += 4;
 
         ecrt_slave_config_sdo8(sc, index, sub, val);
-
-        if (cursor % 8 == 0 && cursor + 2 < size) {
-            ecrt_slave_config_pdo_assign_add(sc, 2, index);
-        }
     }
 
     static const ec_pdo_entry_reg_t domain_regs[] = {
@@ -59,8 +50,20 @@ extern "C" int LLVMFuzzerTestOneInput(const uint8_t *data, size_t size) {
     };
     ecrt_domain_reg_pdo_entry_list(domain, domain_regs);
 
-cleanup:
-    ec_master_clear(&master);
+    // This will now succeed because we are using the fake library
+    if (ecrt_master_activate(master)) {
+        ecrt_release_master(master);
+        return 0;
+    }
 
+    // Run a few cycles of the fake operational state
+    for (int i = 0; i < 5; i++) {
+        ecrt_master_receive(master);
+        ecrt_domain_process(domain);
+        ecrt_domain_queue(domain);
+        ecrt_master_send(master);
+    }
+
+    ecrt_release_master(master);
     return 0;
 }
